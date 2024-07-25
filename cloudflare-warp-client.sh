89b47b33aaca0bf995f7695325b8ab3c62fdc8a7
raihankhan <raihan@appscode.com>
2024-07-25 15:42:38 +0600

    Add license to files
    Signed-off-by: raihankhan <raihankhanraka@gmail.com>

--- cloudflare-warp-client.sh
+++ cloudflare-warp-client.sh
@@ -1,3 +1,17 @@
+# Copyright 2024 Raihan Khan
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Since there is no repo for noble yet the variable $(lsb_release -cs) which returns noble for ubuntu 24.04, will cause issues, use jammy release and it works fine
 # ref:https://community.cloudflare.com/t/warp-on-ubuntu-24-04-lts/652275/3
 VERSION_CODENAME=$(lsb_release -cs)
